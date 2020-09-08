8d9eba43a1751179efd41a80ac5e36ec5c5b6179
shvm61 <60419370+shvm61@users.noreply.github.com>
2020-09-08 11:38:33 +0530

    added middleware

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 import App from "./components/App";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+// const logger = function ({ dispatch, getState }) {
+//   return function (next) {
+//     return function (action) {
+//       console.log("Action_Type=", action.type);
+//       next(action);
+//     };
+//   };
+// };
+
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+  console.log("Action_Type=", action.type);
+  next(action);
+};
+const store = createStore(rootReducer, applyMiddleware(logger));
 // console.log(store.getState());
 ReactDOM.render(
   <React.StrictMode>
